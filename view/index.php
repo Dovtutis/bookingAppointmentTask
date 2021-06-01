ad13be935c9d1e3959c956dd9eb3770b04b76f3a
Dovtutis <dovtutis@gmail.com>
2021-06-01 12:47:27 +0300

    calendar done

--- view/index.php
+++ view/index.php
@@ -47,6 +47,30 @@
                 </div>
             </form>
         </div>
+        <div id="calendar-container">
+            <div id="calendar">
+                <div class="month">
+                    <i class="fas fa-angle-left prev"></i>
+                    <div class="date">
+                        <h1></h1>
+                        <p></p>
+                    </div>
+                    <i class="fas fa-angle-right next"></i>
+                </div>
+                <div class="weekdays">
+                    <div>Sun</div>
+                    <div>Mon</div>
+                    <div>Tue</div>
+                    <div>Wen</div>
+                    <div>Thu</div>
+                    <div>Fri</div>
+                    <div>Sat</div>
+                </div>
+                <div class="days">
+
+                </div>
+            </div>
+        </div>
     </div>
 </main>
 <footer>
@@ -62,6 +86,66 @@
     const lastnameInputEl = document.getElementById('lastname');
     const dayInputEl = document.getElementById('day');
     const timeInputEl = document.getElementById('time');
+    const date = new Date();
+
+    const renderCalendar = () => {
+        date.setDate(1);
+        const firstDayIndex = date.getDay();
+        const months = [
+            "January",
+            "February",
+            "March",
+            "April",
+            "May",
+            "June",
+            "July",
+            "August",
+            "September",
+            "October",
+            "November",
+            "December",
+        ];
+        const monthDays = document.querySelector('.days');
+        const lastDay = new Date(date.getFullYear(), date.getMonth() +1, 0).getDate();
+        const prevLastDay = new Date(date.getFullYear(), date.getMonth(), 0).getDate();
+        const lastDayIndex = new Date(date.getFullYear(), date.getMonth() +1, 0).getDay();
+        const nextDays = 7 -lastDayIndex - 1;
+
+        document.querySelector('.date h1').innerHTML = months[date.getMonth()];
+        document.querySelector('.date p').innerHTML = new Date().toDateString();
+
+        let days = "";
+
+        for (let x = firstDayIndex; x > 0; x--) {
+            days += `<div class="prev-date">${prevLastDay - x + 1}</div>`   
+        }
+
+        for (let i = 1; i <= lastDay; i++) {
+            if (i === new Date().getDate() && date.getMonth() === new Date().getMonth()) {
+                days += `<div class="today">${i}</div>`;
+            }else {
+                days += `<div>${i}</div>`;
+            }
+        }
+
+        for (let j = 1; j <= nextDays; j++) {
+            days += `<div class="next-date">${j}</div>`
+        }
+
+        monthDays.innerHTML = days;
+    }
+
+    document.querySelector('.prev').addEventListener('click', () => {
+        date.setMonth(date.getMonth() - 1);
+        renderCalendar();
+    });
+
+    document.querySelector('.next').addEventListener('click', () => {
+        date.setMonth(date.getMonth() + 1);
+        renderCalendar();
+    });
+
+    renderCalendar();
 
     appointmentFormEl.addEventListener('submit', addAnAppointment);
 
